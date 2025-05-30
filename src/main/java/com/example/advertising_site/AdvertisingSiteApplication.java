b4c5d3a180c2337779c839796ff75c7d152dcefa
puter2 <piotrzajac4@gmail.com>
2025-05-30 23:36:03 +0200

    project start

---
+++ src/main/java/com/example/advertising_site/AdvertisingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.advertising_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisingSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advertising_site/AdvertisingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advertising_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertisingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
